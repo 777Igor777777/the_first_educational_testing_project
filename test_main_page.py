b30ead3843f2a3cf5ba04fc3a128f8297f19204c
Igor Ivashkin <Igor7.0@inbox.ru>
2026-05-25 19:50:05 +0300

    Added check login and register forms

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
+# from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # login_link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
     page.should_be_login_link()
